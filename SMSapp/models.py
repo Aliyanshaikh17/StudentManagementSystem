0c1260c35d9376a5c59893d8d65f636d84b48eaa
Aliyanshaikh17 <aliyanshaikh817@gmail.com>
2025-11-10 14:35:07 +0530

    create a moduls for make table

--- SMSapp/models.py
+++ SMSapp/models.py
@@ -1,16 +1,41 @@
 from django.db import models
 
 class Student(models.Model):
-    StudentID = models.IntegerField(unique=True)
-    Name = models.CharField(max_length=50)
-    ClassName = models.CharField(max_length=10)
-    Section = models.CharField(max_length=1)
-    Gender = models.CharField(max_length=10)
-    Age = models.IntegerField()
-    Phone = models.CharField(max_length=15)
-    City = models.CharField(max_length=50)
-    Percentage = models.FloatField()
-    Attendance = models.IntegerField() 
+    STANDARD_CHOICES = [
+        ('1','1'),
+        ('2','2'),
+        ('3','3'),
+        ('4','4'),
+        ('5','5'),
+        ('6','6'),
+        ('7','7'),
+        ('8','8'),
+        ('9','9'),
+        ('10','10')
+    ]
+    DIVISION_CHOICES = [
+        ('A','A'),
+        ('B','B'),
+        ('C','C'),
+        ('D','D')
+    ]
+    
+
+    student_id = models.IntegerField(unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    standard = models.CharField(max_length=10, choices=STANDARD_CHOICES)
+    division = models.CharField(max_length=1, choices=DIVISION_CHOICES)
+    roll_no = models.IntegerField()
+    address = models.CharField(max_length=150)
+    contact = models.CharField(max_length=15)
+    school_name = models.CharField(max_length=100)
+    class_teacher = models.CharField(max_length=50)
+    favourite_subject = models.CharField(max_length=100)
+    favourite_book = models.CharField(max_length=100)
+    percentage = models.FloatField()
+
+
 
     def __str__(self):
-        return f" {self.Name}"
+        return f" {self.student_id}"
